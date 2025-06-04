0282ea18aac444f2352a5b85d45a037fc157e7e7
Techie <Techie@WL-514.localdomain>
2025-06-04 15:21:35 +0530

    Update from GravityWrite AI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Calculator } from "./components/Calculator";
-import { ThemeProvider } from "./components/theme-provider";
-import { Toaster } from "./components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
+import { RegistrationForm } from "@/components/RegistrationForm";
 
-export default function App() {
+function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <div className="min-h-screen bg-background flex items-center justify-center p-4">
-        <Calculator />
-      </div>
+    <>
+      <RegistrationForm />
       <Toaster />
-    </ThemeProvider>
+    </>
   );
 }
+
+export default App;
